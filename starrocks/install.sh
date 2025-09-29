67d2c9589970b456d2d79205308cbc424e2211c8
Murphy <mofei@starrocks.com>
2025-09-29 20:25:53 +0800

    update result to 4.0.0-rc02
    Signed-off-by: Murphy <mofei@starrocks.com>

--- starrocks/install.sh
+++ starrocks/install.sh
@@ -4,7 +4,7 @@ sudo snap install docker
 sudo apt-get update
 sudo apt-get install -y mysql-client
 
-docker run -p 9030:9030 -p 8030:8030 -p 8040:8040 -itd --name starrocks starrocks/allin1-ubuntu:4.0.0-rc01
+docker run -p 9030:9030 -p 8030:8030 -p 8040:8040 -itd --name starrocks starrocks/allin1-ubuntu:4.0.0-rc02
 
 echo "Starting StarRocks container..."
 sleep 5

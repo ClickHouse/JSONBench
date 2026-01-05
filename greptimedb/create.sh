3eee09c6af105a37cf4fef4bf5dc124139e96111
greptime <luofucong@greptime.com>
2026-01-05 14:30:06 +0800

    x

--- greptimedb/create.sh
+++ greptimedb/create.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-echo "Creating table bluesky..."
-DDL_SQL=$(tr '\n' ' ' < "$DDL_FILE")
+echo "Creating pipeline \"jsonbench\" ..."
+curl -i -X POST 'http://localhost:4000/v1/events/pipelines/jsonbench' -F 'file=@pipeline.yaml'
+
+echo "Creating table \"bluesky\" ..."
+DDL_SQL=$(tr '\n' ' ' < ddl.sql)
 curl -i -X POST -H 'Content-Type: application/x-www-form-urlencoded' \
     http://localhost:4000/v1/sql \
     -d "sql=$DDL_SQL" \
